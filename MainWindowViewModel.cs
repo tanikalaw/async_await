4ae81197d43e74d967bed86ff9c7fffdca8ce454
Rey Joseph Samonte <rj.samonte@godigitalcorp.com>
2020-09-30 21:22:45 +0800

    Initial Commit

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+
+namespace Async_and_Await
+{
+   public class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+
+        }    
+
+        private async Task DownloadWebsite()
+        {
+            Task.Run(() => "return");
+        }
+
+
+    }
+}
